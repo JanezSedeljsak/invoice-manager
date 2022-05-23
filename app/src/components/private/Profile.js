695382c639851db33b3016f50b503fcb11c91e2a
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-05-23 16:11:30 +0200

    ui improvements

--- app/src/components/private/Profile.js
+++ app/src/components/private/Profile.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 function Profile() {
     return (
         <form className={"ui form"}>
+            <h2>New credentials</h2>
             <div className={"field"}>
                 <label>Fullname</label>
                 <input type="text" name="fullname" placeholder="Fullname" />
@@ -15,7 +16,21 @@ function Profile() {
                 <label>Password</label>
                 <input type="text" name="password" placeholder="Password" />
             </div>
-            <button className={"ui primary button"} type="submit">Register</button>
+            <div className={"field"}>
+                <label>Profile image</label>
+                <input type="file" name="image" />
+            </div>
+            <hr/>
+            <h2>Old credentials</h2>
+            <div className={"field"}>
+                <label>Email</label>
+                <input type="text" name="old_email" placeholder="Old email" />
+            </div>
+            <div className={"field"}>
+                <label>Password</label>
+                <input type="text" name="old_password" placeholder="Old password" />
+            </div>
+            <button className={"ui primary button"} type="submit">Update profile</button>
         </form>
     );
 }
--- app/src/components/public/Home.js
+++ app/src/components/public/Home.js
@@ -5,20 +5,25 @@ import Waves from '../blocks/Waves';
 
 function Home() {
     return (
-        <>
+        <div id="home-container">
             <Waves />
-            <div className="ui cards">
-                <div className="card">
-                    <div className="content">
-                        <div className="header">Elliot Fu</div>
-                        <div className="meta">Friend</div>
-                        <div className="description">
-                            Elliot Fu is a film-maker from New York.
+            <div className="home-content">
+                <div className="ui cards">
+                    <div className="card">
+                        <div className="content">
+                            <div className="header">Elliot Fu</div>
+                            <div className="meta">Friend</div>
+                            <div className="description">
+                                Elliot Fu is a film-maker from New York.
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className='home-footer'>
+                    <p>hello</p>
+                </div>
             </div>
-        </>
+        </div>
 
     );
 }
