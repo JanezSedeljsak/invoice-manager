6b9f3ab7e6d2bf64a739a6480caa703856f0fd0e
Janez Sedeljšak <janez.sedeljsak@pro-bit.si>
2022-11-10 10:48:59 +0100

    port changed to 5055 to avoid adb problems

--- app/src/requests/index.js
+++ app/src/requests/index.js
@@ -1,4 +1,4 @@
-const API_URI = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:8000' : '';
+const API_URI = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'http://localhost:5055' : '';
 
 function generateFormData(jsonData) {
     const form_data = new FormData();
